d149a2275f696c5180737cc8f96df30dc493b550
sebousan <sebousan@gmail.com>
2023-10-06 16:54:29 +0200

    settings

--- admin/app/settings/available-collections.js
+++ admin/app/settings/available-collections.js
@@ -1,9 +1,11 @@
 import realestates from '../content/realestates.js'
+import realestates_types from '../content/realestates_types.js'
 import places from '../content/places.js'
 import persons from '../content/persons.js'
 
 export default [
     realestates,
+    realestates_types,
     places,
     persons
 ]
